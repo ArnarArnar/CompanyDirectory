25ed24199ddea3f485755092c0923e53dee29b0a
Arnar Arnarson <arnararnarson@gmail.com>
2021-05-30 21:24:25 +0000

    Minor styling changes

--- src/components/CompanyItem.js
+++ src/components/CompanyItem.js
@@ -2,23 +2,31 @@ import React from 'react';
 import starThin from '../assets/img/star-thin.svg';
 
 export default function CompanyItem({ company }) {
+    var splitAddress;
+    if (company.address.includes(',')) {
+        splitAddress = company.address.split(',');
+    } else {
+        splitAddress = company.address.split('  ');
+    }
+
     return (
         <div className="w-full ">
-            <div id="card" className="flex p-4 mb-2 bg-gray-200 rounded-lg">
+            <div id="card" className="flex px-3 py-2 mb-2 bg-gray-200 rounded-lg">
                 <div className="flex flex-col justify-between w-full ">
                     <div className="flex flex-row ">
-                        <div className="self-center flex h-0 p-1.5 bg-green-500 border-2 rounded-full mr-2"></div>
+                        {/* <div className="self-center flex h-0 p-1.5 bg-green-500 border-2 rounded-full mr-2"></div> */}
                         <div className="pr-1 pt-0.5 text-lg font-bold text-blue700">
                             {company.name}
                         </div>
                     </div>
                     <div className="flex justify-between mt-1 ">
-                        <div className="text-blue700">
-                            <div> {company.name}</div>
-                            <div>200 Kópavogur</div>
+                        <div className="flex flex-col justify-end text-blue700">
+                            <div> {splitAddress[0]}</div>
+                            <div> {splitAddress[1]}</div>
+
                             <div>
                                 <span className="font-medium">kt: </span>
-                                <span>3342423543543</span>
+                                <span>{company.sn}</span>
                             </div>
                         </div>
 
