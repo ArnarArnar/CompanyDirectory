deca6035c1fa214580a282ea2d7cd8c2e4495546
Arnar Arnarson <arnararnarson@gmail.com>
2021-05-30 15:17:06 +0000

    Initial commit, base configuration

---
+++ stylelint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: ['stylelint-config-standard'],
+    plugins: ['stylelint-order'],
+    rules: {
+        'at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen']
+            }
+        ],
+        'declaration-block-trailing-semicolon': null,
+        'no-descending-specificity': null,
+        ignoreFiles: ['./src/assets/styles.css', './src/assets/tailwind.css']
+    }
+};
